be7b87158a09b844e04acee213722adeca9ac7f5
Dimitar <141-dkolev@users.noreply.gitlab-talentboost.vmware.com>
2020-03-30 11:56:47 +0300

    Removed Refactoring Project

--- TalentBoost Refactoring/src/exercise3.java
+++
@@ -1,9 +0,0 @@
-public void calculateCartTotal(Cart cart) {
-	int discountPercent = cart.getDiscount();
-	double discountAmount = (discountPercent / 100) * cart.getTotal();
-
-	return cart.total - discountAmount;
-}
-
-//Коментар:
-//Този метод е ненужен, тъй като можем просто да си сметнем тотала в метода cart.getTotal()
