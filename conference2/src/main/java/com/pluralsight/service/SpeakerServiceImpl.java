89cff0bd118be4673dd6b7a4a6f900f219035a31
Димитър Колев <dimitarkolev0911@gmail.com>
2020-08-18 15:05:58 +0300

    Added SpeakerService interface and SpeakerServiceImpl implementation of the interface

--- conference2/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ conference2/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,5 +1,19 @@
 package com.pluralsight.service;
 
-public class SpeakerServiceImpl {
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repisotiry.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repisotiry.SpeakerRepository;
+
+import java.util.List;
+
+public class SpeakerServiceImpl implements SpeakerService {
+
+    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    public List<Speaker> findAll()
+    {
+        return repository.findAll();
+    }
+
 
 }
