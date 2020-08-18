d7b7f2f087598c297bb464befd7129310393a586
Димитър Колев <dimitarkolev0911@gmail.com>
2020-08-18 16:06:35 +0300

    Adding the configuration file

---
+++ conference2-java/src/main/java/AppConfig.java
@@ -0,0 +1,13 @@
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService(){
+        return new SpeakerServiceImpl();
+    }
+}
