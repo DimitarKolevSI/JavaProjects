22fc6a0639bf0ec0fcce83415104b0bc2d84c14a
Димитър Колев <dimitarkolev0911@gmail.com>
2020-08-21 15:40:52 +0300

    Refactored SessionRepository adding DSL to the getSessionThatHaveName method

--- ps-spring-data-jpa-master/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
+++ ps-spring-data-jpa-master/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -3,6 +3,8 @@ package com.pluralsight.conferencedemo.repositories;
 import com.pluralsight.conferencedemo.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SessionJpaRepository extends JpaRepository<Session, Long> {
+import java.util.List;
 
+public interface SessionJpaRepository extends JpaRepository<Session, Long> {
+    List<Session> findBySessionNameContains(String name);
 }
