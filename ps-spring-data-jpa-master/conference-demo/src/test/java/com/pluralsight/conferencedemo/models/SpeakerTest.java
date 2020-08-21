5e5855765dbea552fdd0b81d6cc9ea2f7e1c2a29
Димитър Колев <dimitarkolev0911@gmail.com>
2020-08-21 13:52:51 +0300

    Inital commit and created SpeakerJpaRepository

---
+++ ps-spring-data-jpa-master/conference-demo/src/test/java/com/pluralsight/conferencedemo/models/SpeakerTest.java
@@ -0,0 +1,51 @@
+package com.pluralsight.conferencedemo.models;
+
+import com.pluralsight.conferencedemo.repositories.SpeakerJpaRepository;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class SpeakerTest {
+    @Autowired
+    private SpeakerJpaRepository repository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    public void testFind() throws Exception {
+        Speaker speaker = repository.getOne(1L);
+        assertNotNull(speaker);
+    }
+
+    @Test
+    @Transactional
+    public void testSaveAndGetAndDelete() throws Exception {
+        Speaker s = new Speaker();
+        s.setCompany("Pluralsight");
+        s.setFirstName("Dan");
+        s.setLastName("Bunker");
+        s.setTitle("Author");
+        s.setSpeakerBio("Consulting and mentoring");
+        s = repository.saveAndFlush(s);
+
+        // clear the persistence context so we don't return the previously cached location object
+        // this is a test only thing and normally doesn't need to be done in prod code
+        entityManager.clear();
+
+        Speaker otherSpeaker = repository.getOne(s.getSpeakerId());
+        assertEquals("Dan", otherSpeaker.getFirstName());
+
+        repository.deleteById(otherSpeaker.getSpeakerId());
+    }
+
+}
