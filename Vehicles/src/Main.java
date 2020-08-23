bae42aa94fbcbc618c6193df436a91d5a4d82c8b
Димитър Колев <dimitarkolev0911@gmail.com>
2020-08-23 13:36:20 +0300

    Added few methods to TimeSlotsJpaRepository

--- Vehicles/src/Main.java
+++ Vehicles/src/Main.java
@@ -1,4 +1,5 @@
 import Vehicles.CargoVehicle;
+import Vehicles.Gender;
 import Vehicles.PassengerVehicle;
 import Vehicles.Vehicle;
 
@@ -18,17 +19,22 @@ public class Main {
         return passengers;
     }
 
+
     public static void main(String[] args){
-        PassengerVehicle p = new PassengerVehicle(500, 20);
-        PassengerVehicle p1 = new PassengerVehicle(600,30);
-        CargoVehicle c = new CargoVehicle(600,400);
-        CargoVehicle c1 = new CargoVehicle(700,500);
-        Vehicle[] vehicles = new Vehicle[4];
-        vehicles[0] = c;
-        vehicles[1] = c1;
-        vehicles[2] = c;
-        vehicles[3] = c1;
-        System.out.println(pickPassengersWithVehicles(vehicles, 4,10));
+//        PassengerVehicle p = new PassengerVehicle(500, 20);
+//        PassengerVehicle p1 = new PassengerVehicle(600,30);
+//        CargoVehicle c = new CargoVehicle(600,400);
+//        CargoVehicle c1 = new CargoVehicle(700,500);
+//        Vehicle[] vehicles = new Vehicle[4];
+//        vehicles[0] = c;
+//        vehicles[1] = c1;
+//        vehicles[2] = c;
+//        vehicles[3] = c1;
+//        System.out.println(pickPassengersWithVehicles(vehicles, 4,10));
+
+
+        if(Gender.Male == Gender.valueOf("Male")) System.out.println("True");
+        else System.out.println("False");
 
     }
 }
--- ps-spring-data-jpa-master/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TimeSlotsJpaRepository.java
+++ ps-spring-data-jpa-master/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TimeSlotsJpaRepository.java
@@ -1,4 +1,14 @@
 package com.pluralsight.conferencedemo.repositories;
 
-public interface TimeSlotsJpaRepository {
+import com.pluralsight.conferencedemo.models.TimeSlots;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
+public interface TimeSlotsJpaRepository extends JpaRepository<TimeSlots,Long> {
+    List<TimeSlots> findByStartTimeAfter(Time time);
+    List<TimeSlots> findByTimeSlotDateAfter(Date date);
+    List<TimeSlots> findByTimeSlotDateBefore(Date date);
 }
