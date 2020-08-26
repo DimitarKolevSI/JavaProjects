17ebc24ba9803f915a39b417a12d79131cfe87f8
Димитър Колев <dimitarkolev0911@gmail.com>
2020-08-26 11:33:03 +0300

    Added some methods for the BookJpaRepository

---
+++ LibraryProject/Library/src/main/java/com/library/repositories/BookJpaRepository.java
@@ -0,0 +1,15 @@
+package com.library.repositories;
+
+import com.library.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookJpaRepository extends JpaRepository<Book,Long> {
+    List<Book> findByAuthor(String author);
+    Book findByTitle(String title);
+    List<Book> findByGenre(String genre);
+    List<Book> findByPagesLessThan(Integer pages);
+    List<Book> findByPagesGreaterThan(Integer pages);
+
+}
