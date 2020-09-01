5be60e5e27bbf5fae4529301a30133ae5f47115c
Димитър Колев <dimitarkolev0911@gmail.com>
2020-09-01 15:51:04 +0300

    Added RatedBooks entity

---
+++ LibraryProject/Library/src/main/java/com/library/models/RatedBooks.java
@@ -0,0 +1,57 @@
+package com.library.models;
+
+import javax.persistence.*;
+
+@Entity
+public class RatedBooks {
+
+    @EmbeddedId
+    RatedBooksKey id;
+
+    @ManyToOne
+    @MapsId("username")
+    @JoinColumn(name = "username")
+    Reader reader;
+
+    @ManyToOne
+    @MapsId("booksId")
+    @JoinColumn(name = "books_id")
+    Book book;
+
+    double rating;
+
+    public RatedBooks() {
+    }
+
+    public RatedBooksKey getId() {
+        return id;
+    }
+
+    public void setId(RatedBooksKey id) {
+        this.id = id;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
