eaafa6cf0c917bec85c6516baec1211d89ccde33
Dimitar-Kolev-99 <dimitarrukolev@gmail.com>
2020-10-31 09:51:17 +0200

    Initial commit for the Netflix task

---
+++ MJT_Week3/src/bg/sofia/uni/fmi/mjt/netflix/Main.java
@@ -0,0 +1,8 @@
+package bg.sofia.uni.fmi.mjt.netflix;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ MJT_Week3/src/bg/sofia/uni/fmi/mjt/netflix/content/enums/Genre.java
@@ -0,0 +1,4 @@
+package bg.sofia.uni.fmi.mjt.netflix.content.enums;
+
+public enum Genre {
+}
